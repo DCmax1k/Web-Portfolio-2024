45d5429596030640c6f12e4adec8a68cbb337911
Dylan Caldwell <dylan.caldwell35@gmail.com>
2024-04-22 22:49:42 -0400

    Landing page finished

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -5,20 +5,57 @@ class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
-
+            imgLoaded: false,
+            sideBar: false,
         }
+
+        this.toggleSideBar = this.toggleSideBar.bind(this);
+    }
+
+    componentDidMount() {
+        const img = new Image();
+        img.src = '/images/renderedBgBright.png';
+        img.onload = () => {
+            this.setState({ imgLoaded: true });
+        };
+    }
+
+    toggleSideBar() {
+        this.setState({
+            sideBar: !this.state.sideBar,
+        });
     }
 
 
     render() {
         return (
             <div className='Index'>
+
+                {/* SIDEBAR */}
+                <div id='sideBar' className={this.state.sideBar?'active':''}>
+                    <img className='closeX' onClick={this.toggleSideBar} alt='Close sidebar' src='/images/closeX.svg' />
+
+                    <div className='links'>
+                        <a onClick={this.toggleSideBar} href='#landing'>// home</a>
+                        <a onClick={this.toggleSideBar} href='#expertise'>// expertise</a>
+                        <a onClick={this.toggleSideBar} href='#work'>// work</a>
+                        <a onClick={this.toggleSideBar} href='#experience'>// experience</a>
+                        <a onClick={this.toggleSideBar} href='#contact'>// contact</a>
+                    </div>
+
+                    <div className='cr'>
+                    © 2024. Made by Dylan Caldwell.
+                    <br />
+                    All right reserved.
+                    </div>
+                </div>
+
                 {/* LANDING */}
                 <div id="landing">
-                    <img id='bg' alt='background' src='/images/renderedBgBright.png' />
+                    <img className={this.state.imgLoaded?'active':''} id='bg' alt='background' src='/images/renderedBgBright.png' />
 
                     <div className='topLeft'>
+                        <img onClick={this.toggleSideBar} className='hamMenu' alt='Side bar open' src='/images/hamMenu.svg' />
                         <h2 className='domain'>DigitalCaldwell</h2>
                     </div>
 
